ddc275df1a16652d56e24e9298b2eb1b4d4d6669
xMAXSIMKAx <maxymram@gmail.com>
2024-02-17 10:22:53 +0100

    Initial commit

---
+++ src/App/Product.java
@@ -0,0 +1,31 @@
+package App;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public String toString() {
+        return String.format("Product: %s, Price: %.2f, Quantity: %d", name, quantity, price);
+
+
+    }
+}
